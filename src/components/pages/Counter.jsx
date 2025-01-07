0a59de262faa4690b97da9c26bf98e8c22401e56
jathuRSHAN <sjathurshan19980419@gmail.com>
2025-01-07 15:43:51 +0530

    test2

--- src/components/pages/Counter.jsx
+++ src/components/pages/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { project } from '../data/dummydata'
-// import CountUp from 'react-countup'
+import CountUp from 'react-countup'
 //import Heading from '../common/Heading'
 
 export default function Counter() {
@@ -12,7 +12,7 @@ export default function Counter() {
                         <div className="box">
                             {/* <i>{item.icon}</i> */}
                             <h1 className='heading'>
-                                {/* <CountUp enableScrollSpy duration={2} end={item.num} /> */}
+                                <CountUp enableScrollSpy duration={2} end={item.num} />
                             </h1>
                             <h3>{item.title}</h3>
                         </div>
